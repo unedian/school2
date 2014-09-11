deb2ea5389d8ec1a53cf7f735c7c1e206ed6d4bb
unedian <goldend4gon@otmail.com>
2014-09-11 15:05:17 +0200

    opdracht 3

---
+++ php/onevengetallen.php
@@ -0,0 +1,20 @@
+<?php	
+
+$getal = $_GET['getal'];
+if ($getal % 2 == 0) {
+	echo $getal . " is even";
+} elseif ($getal % 1 == 0) { 
+	echo $getal . " is oneven";
+}
+
+<<<<<<< HEAD
+
+?>
+=======
+oneven();
+<<<<<<< HEAD
+?>
+>>>>>>> origin/master
+=======
+?>
+>>>>>>> parent of 293eda0... Update onevengetallen.php
