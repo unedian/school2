aaea68cd77d787c423da8621b7138e10010b2d92
unedian <goldend4gon@otmail.com>
2014-09-11 15:48:34 +0200

    opdracht 4

---
+++ php/priemgetallen.php
@@ -0,0 +1,17 @@
+<?php	
+
+$getal = $_GET['getal'];
+
+ if ($getal % 2 == $getal % 2) { 
+	echo $getal . " is geen priemgetal";
+} 
+
+
+elseif ($getal % $getal == 1) {
+	echo $getal . " is priemgetal";
+}
+
+
+
+
+?>
